b13bdf1838d2847970b77c36e9e313c9ebeefe04
Rajeshwari Dhandare <125365753+RDhandare09@users.noreply.github.com>
2023-07-12 01:54:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let cal = document.getElementById('cal');
+
+buttons = document.querySelectorAll('button');
+
+let calValue = '';
+
+for (item of buttons) 
+{
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+
+        if (buttonText == 'X')
+         {
+            buttonText = '*';
+            calValue += buttonText;
+            cal.value = calValue;
+         }
+
+        else if (buttonText == 'Clr')
+         {
+            calValue = "";
+            cal.value = calValue;
+         }
+
+        else if (buttonText == '=') 
+        {
+            cal.value = eval(calValue);
+        }
+
+        else
+        
+        {
+            calValue += buttonText;
+            cal.value = calValue;
+        }
+
+    })
+}
+
